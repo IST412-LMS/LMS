c209fc97c8d36ef166786ee90f45762b135920df
Hannah <hvk5388@psu.edu>
2022-10-11 17:33:53 -0400

    edit testharness

--- testHarness/TestHarness.java
+++ testHarness/TestHarness.java
@@ -4,6 +4,8 @@
  */
 package learningmanagementsystem.testHarness;
 
+import java.io.Console;
+
 import learningmanagementsystem.Authentification.LoginController;
 
 /**
@@ -12,8 +14,37 @@ import learningmanagementsystem.Authentification.LoginController;
  */
 public class TestHarness {
     public static void main(String[] args) {
+        Console console = System.console();
+        if (console == null) {
+            System.err.println("No console.");
+            System.exit(1);
+        }
+        // while (true) {
+
+        // Pattern pattern =
+        // Pattern.compile(console.readLine("%nEnter your regex: "));
+
+        // Matcher matcher =
+        // pattern.matcher(console.readLine("Enter input string to search: "));
+
+        // boolean found = false;
+        // while (matcher.find()) {
+        // console.format("I found the text" +
+        // " \"%s\" starting at " +
+        // "index %d and ending at index %d.%n",
+        // matcher.group(),
+        // matcher.start(),
+        // matcher.end());
+        // found = true;
+        // }
+        // if(!found){
+        // console.format("No match found.%n");
+        // }
+        // }
+
         // calls the login controller
         LoginController lc = new LoginController();
+
         // faculty uploading grade
         // 1. FACULTY, facIST123, 1, 4
 
