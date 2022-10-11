52c7fdafc9a1df39c301871a09a7acc9b854a905
hvk5388 <hvk5388@psu.edu>
2022-10-11 21:48:12 +0100

    add testharness

---
+++ testHarness/TestHarness.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package learningmanagementsystem.testHarness;
+import learningmanagementsystem.Authentification.LoginController;
+
+/**
+ *
+ * @author hkgob
+ */
+public class TestHarness {
+        public static void main(String[] args){
+        LoginController lc = new LoginController();
+    }
+}
