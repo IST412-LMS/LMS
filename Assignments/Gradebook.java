ec6c6bc7016dc1a6dc4b4ea13acd00a6601c10b9
mrb6256 <89985162+mrb6256@users.noreply.github.com>
2022-11-02 15:38:16 -0400

    Create Gradebook.java

---
+++ Assignments/Gradebook.java
@@ -0,0 +1,14 @@
+package learningmanagementsystem.Assignments;
+
+public class Gradebook {
+
+    public void gradeWeighting() {
+        // display names and weights of categories
+        System.out.println("Class categories and grade weights:");
+        System.out.println("Assignments- 20%");
+        System.out.println("2. Tests and Quizzes- 25%");
+        System.out.println("3. In-cLass Activities- 20%");
+        System.out.println("4. Presentations and Essays- 20%");
+        System.out.println("5. Participation- 15%");
+        }
+    }
