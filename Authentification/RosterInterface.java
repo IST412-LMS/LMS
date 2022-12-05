5e62aebb3bd9c3281a6bcf7b475cc447f7930288
njd5435 <89604655+njd5435@users.noreply.github.com>
2022-12-04 20:01:48 -0500

    Update RosterInterface.java

--- Authentification/RosterInterface.java
+++ Authentification/RosterInterface.java
@@ -1,1 +1,21 @@
+package learningmanagementsystem.Authentification;
+
+import java.util.Scanner;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class RosterInterface extends JFrame implements ActionListener {
+
+}
 
